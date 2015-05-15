e6d867d7c193b06e53d08a5f6b634eb0f84ee3cb
conrad <conrad@pythonanywhere.com>
2015-05-15 19:40:39 +0000

    processed oauth callback to get token and user email

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, redirect, request
-from requests import Request
+import requests
+import os
 
 
 app = Flask(__name__)
@@ -11,14 +12,36 @@ def redirect_to_google_oauth_login():
         'response_type': 'code',
         'client_id': os.environ['GOOGLE_OAUTH_CLIENT_ID'],
         'redirect_uri': 'https://conrad.pythonanywhere.com/oauth2callback',
-        'scope': 'email',
+        # for the scopes, check it out at https://developers.google.com/oauthplayground/
+        'scope': 'https://www.googleapis.com/auth/userinfo.email',
+        ## 'access_type': 'offline',
+        'state': 'lalalalal1',
     }
-    final_url = Request(url=redirect_url, params=query_params).prepare().url
+    final_url = requests.Request(url=redirect_url, params=query_params).prepare().url
     return redirect(final_url)
 
 @app.route('/oauth2callback')
-def google_callback():
+def google_oauth2_authcode_callback():
     if 'error' in request.args:
         return Response('oh so sad')
-    else:
-        return Response('cool- got key ending with {}'.format(request.args['code'][-5:]))
+    # get access token and refresh token using the auth code
+    auth_code = request.args['code']
+    # check your google config json for which link to swap auth-code for token
+    oauth_link = 'https://accounts.google.com/o/oauth2/token'
+    post_params = {
+        'grant_type': 'authorization_code',
+        'code': auth_code,
+        'client_id': os.environ['GOOGLE_OAUTH_CLIENT_ID'],
+        'client_secret': os.environ['GOOGLE_OAUTH_CLIENT_SECRET'],
+        'redirect_uri': 'https://conrad.pythonanywhere.com/oauth2callback',
+        # the same redirect_uri used to generate the authcode
+    }
+    result = requests.post(oauth_link, data=post_params).json()
+    access_token = result['access_token']  # also has other stuff like token_id
+
+    google_plus_user_info_api = 'https://www.googleapis.com/userinfo/v2/me'
+    hidden_headers = {'Authorization': 'Bearer {}'.format(access_token)}
+    user_info = requests.get(google_plus_user_info_api, headers=hidden_headers).json()
+    return Response('cool- your email is {}'.format(user_info['email']))
+
+
