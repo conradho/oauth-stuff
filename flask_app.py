94b35d4c171fd7396a2b3e79411fa0fe5e915923
conrad <conrad@pythonanywhere.com>
2015-07-29 10:46:51 +0000

    added some comments to google oauth flask app

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,8 @@
+"""
+references:
+    to understand api calls https://developers.google.com/oauthplayground/
+    setup project > api & auth > oauth credentials at https://console.developers.google.com
+"""
 from flask import Flask, Response, redirect, request
 import requests
 import os
@@ -5,8 +10,10 @@ import os
 
 app = Flask(__name__)
 
+# this is the initial page for the enduser to navigate to
 @app.route('/')
 def redirect_to_google_oauth_login():
+    # download json file from google console oauth credentials page to see what the auth_uri is
     redirect_url = 'https://accounts.google.com/o/oauth2/auth'
     query_params = {
         'response_type': 'code',
@@ -22,11 +29,17 @@ def redirect_to_google_oauth_login():
 
 @app.route('/oauth2callback')
 def google_oauth2_authcode_callback():
+    """
+    this url is the redirect URI that you setup on google developers console
+    google hits this after enduser has given permission, and first gives you an auth code
+    you take this auth code and swap it for an access token (and also maybe a refresh token)
+    """
     if 'error' in request.args:
         return Response('oh so sad')
-    # get access token and refresh token using the auth code
+
     auth_code = request.args['code']
-    # check your google config json for which link to swap auth-code for token
+
+    # download json file from google console oauth credentials page to see what the token_uri is
     oauth_link = 'https://accounts.google.com/o/oauth2/token'
     post_params = {
         'grant_type': 'authorization_code',
@@ -34,11 +47,13 @@ def google_oauth2_authcode_callback():
         'client_id': os.environ['GOOGLE_OAUTH_CLIENT_ID'],
         'client_secret': os.environ['GOOGLE_OAUTH_CLIENT_SECRET'],
         'redirect_uri': 'https://conrad.pythonanywhere.com/oauth2callback',
-        # the same redirect_uri used to generate the authcode
+        # the same redirect_uri that google just hit
     }
+
     result = requests.post(oauth_link, data=post_params).json()
     access_token = result['access_token']  # also has other stuff like token_id
 
+    # now you can hit various google api's that you have permission for to get info
     google_plus_user_info_api = 'https://www.googleapis.com/userinfo/v2/me'
     hidden_headers = {'Authorization': 'Bearer {}'.format(access_token)}
     user_info = requests.get(google_plus_user_info_api, headers=hidden_headers).json()
